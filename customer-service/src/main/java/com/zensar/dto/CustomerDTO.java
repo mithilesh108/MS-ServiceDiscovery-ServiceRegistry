c742ee49c2fc24b2b1a55b6aa82203427c3b11df
mithilesh108 <tech.mithilesh108@gmail.com>
2024-06-21 14:26:49 +0530

    customer service

---
+++ customer-service/src/main/java/com/zensar/dto/CustomerDTO.java
@@ -0,0 +1,14 @@
+package com.zensar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class CustomerDTO {
+    int id;
+    String name;
+    String address;
+}
